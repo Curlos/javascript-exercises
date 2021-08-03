9231bfb304e2912e62e82938433d9284c5a45ece
Carlos Martinez <curlosmart@gmail.com>
2021-08-03 15:17:05 -0400

    findTheOldest (complete)

--- findTheOldest/findTheOldest.js
+++ findTheOldest/findTheOldest.js
@@ -1,5 +1,25 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    let oldestPerson = {}
+    let oldestAge = 0
+    let yearToday = new Date().getFullYear()
 
+    people.forEach((person) => {
+        if ('yearOfDeath' in person) {
+            let personAge = person.yearOfDeath - person.yearOfBirth
+            if (personAge > oldestAge) {
+                oldestPerson = person
+                oldestAge = personAge
+            }
+        } else {
+            let personAge = yearToday - person.yearOfBirth
+            if (personAge > oldestAge) {
+                oldestPerson = person
+                oldestAge = personAge
+            }
+        }
+    })
+
+    return oldestPerson
 };
 
 module.exports = findTheOldest;
